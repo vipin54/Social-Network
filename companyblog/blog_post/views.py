22ff87778e97340ac7a4ad86f288b296d20e9137
Vipin Yadav <vipinyadav546@gmail.com>
2021-06-16 02:25:47 +0530

    added some final html files

--- companyblog/blog_post/views.py
+++ companyblog/blog_post/views.py
@@ -37,7 +37,7 @@ def blog_post(blog_post_id):
 
 #update to a blog post
 
-@blog_posts.route('/<int:blog_post_id/update>',methods=['GET,''POST'])
+@blog_posts.route('/<int:blog_post_id>/update',methods=['GET,''POST'])
 @login_required
 def update(blog_post_id):
 	blog_post=BlogPost.query.get_or_404(blog_post_id)
@@ -58,7 +58,7 @@ def update(blog_post_id):
 		return redirect(url_for('blog_posts.blog_post',blog_post_id=blog_post.id))
 
 
-	elif request.method = 'GET':
+	elif request.method == 'GET':
 		form.title.data=blog_post.title
 		form.text.data=blog_post.text
 
@@ -67,7 +67,7 @@ def update(blog_post_id):
 
 
 # delete a blog post
-@blog_posts.route('/<int:blog_post_id/delete>',methods=['GET,''POST'])
+@blog_posts.route('/<int:blog_post_id>/delete',methods=['GET,''POST'])
 @login_required
 def delete_post(blog_post_id):
 	blog_post=BlogPost.query.get_or_404(blog_post_id)
--- companyblog/core/views.py
+++ companyblog/core/views.py
@@ -1,10 +1,15 @@
+from companyblog.models import BlogPost
 from flask import render_template,request,Blueprint
+
 
 core=Blueprint('core',__name__)
 
 @core.route('/')
 def index():
-	return render_template('index.html') 
+
+	page=request.args.get('page',1,type=int)
+	blog_posts= BlogPost.query.order_by(BlogPost.date.desc()).paginate(page=page, per_page=5)
+	return render_template('index.html',blog_posts=blog_posts) 
 
 
 @core.route('/info')
